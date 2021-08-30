1ef159c9c2f591ac87d7f0a8aa97c468057ecec8
theflyingrowan <78631024+theflyingrowan@users.noreply.github.com>
2021-08-30 16:27:28 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,118 +5,139 @@
 
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
+//==========================
+// import the sync-fix module.
+//==========================
+import { UserSyncFix } from './sync-fix'
+
 /**
  * The main class of this app. All the logic goes here.
  */
 export default class HelloWorld {
-	private kitSound: MRE.Actor = null;
+	private kitItem: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 
-		//========================
-	// Trigger volume object
-	//========================
-	private triggerVolume: MRE.Actor;
-
-	//========================
-	// An Actor to interface with the trigger volume
-	//========================
-	private hat: MRE.Actor;
+	//==========================
+	// Declare a syncfix attribute to handle the synchronization fixes.
+	// In this case, syncfix will call the synchronization functions
+	// no more than once every 5000 ms (1 sec).
+	//==========================
+	private syncfix = new UserSyncFix(5000);
 
 	/*
-	 * constructor
+	 * Track which attachments belongs to which user
+	 * NOTE: The MRE.Guid will be the ID of the user.  Maps are more efficient with Guids for keys
+	 * than they would be with MRE.Users.
 	 */
+	private attachments = new Map<MRE.Guid, MRE.Actor>();
+
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
+		//this.context.onUserJoined((user) => this.userJoined(user));
+
+		/*
+		 * Set up a userLeft() callback
+		 */
+		this.context.onUserLeft((user) => this.userLeft(user));
 	}
+
+	//==========================
+	// Synchronization function for attachments
+	// Need to detach and reattach every attachment
+	//==========================
+	private synchronizeAttachments() {
+		// Loop through all values in the 'attachments' map
+		// The [key, value] syntax breaks each entry of the map into its key and
+		// value automatically.  In the case of 'attachments', the key is the
+		// Guid of the user and the value is the actor/attachment.
+		for (const [userId, attachment] of this.attachments) {
+			// Store the current attachpoint.
+			const attachPoint = attachment.attachment.attachPoint;
+
+			// Detach from the user
+			attachment.detach();
+
+			// Reattach to the user
+			attachment.attach(userId, attachPoint);
+		}
+	}
+
 
 	/**
 	 * Once the context is "started", initialize the app.
 	 */
 	private started() {
-		/*
-		* set up somewhere to store loaded assets (meshes, textures,
-		* animations, gltfs, etc.)
-		*/
+		// set up somewhere to store loaded assets (meshes, textures,
+		// animations, gltfs, etc.)
 		this.assets = new MRE.AssetContainer(this.context);
 
-		//============================
-		// Create the trigger area.
-		// It will be a simple box primitive.
-		//============================
-		this.triggerVolume = MRE.Actor.CreatePrimitive(this.assets,
-			{
-				definition: { shape: MRE.PrimitiveShape.Box },
-				actor: {
-					transform: {
-						local: {
-							scale: { x: 0.5, y: 1, z: 1.75 }
-						}
-					},
-					appearance: { enabled: false }
-				},
-				addCollider: true		/* Must have a collider for triggers. */
-			}
-		);
-
-		//============================
-		// Set the collider to be a trigger.
-		// Note: UNDOCUMENTED BEHAVIOR: This must be done *after* the call to
-		// create the actor.
-		//============================
-		this.triggerVolume.collider.isTrigger = true;
-
-		// When an Actor enters the trigger volume, spawn a copy of a kit item
-		this.triggerVolume.collider.onTrigger('trigger-enter', (actor) => this.kitItem());
-
-		//============================
-		// Create an Actor to interface with the trigger volume.  Make the Actor
-		// wearable.
-		//============================
-
-		this.hat = MRE.Actor.CreateFromLibrary(this.context, {
-			// attach an item to the user
-			// Assign the return value of CreateFromLibrary() to a variable.
-			//the number below is the item's artifact id.
-			resourceId: 'artifact:1725170080645382585',
-			actor: {
-				attachment: {
-					attachPoint: 'head'
-				},
-				transform: {
-					local: {
-						position: { x: 0.0, y: 0.2, z: 0.0 }
-					}
-				},
-				rigidBody: {
-					useGravity: false	// Don't let the actor fall when released.
-				},
-				collider: {
-					geometry: {
-						shape: MRE.ColliderType.Box,
-						size: { x: 0.1, y: 0.1, z: 0.1 }
-					}
-				}
-			}
-		});
-	}
-
-	private userJoined(user: MRE.User) {
-		// print the user's name to the console
-		console.log(`${user.name} joined`)
-	}
-
-	/*
-	* Callback for when the trigger volume is entered
-	*/
-	//=====================
-	// Removed unused MRE.User parameter.  For this example, it would have needed to
-	// be an MRE.Actor.
-	//=====================
-	private kitItem() {
-		/*Spawn the kit item */
-		this.kitSound = MRE.Actor.CreateFromLibrary(this.context, {
+		// spawn a copy of a kit item
+		this.kitItem = MRE.Actor.CreateFromLibrary(this.context, {
 			// the number below is the item's artifact id.
 			resourceId: 'artifact:1812869470075486548'
 		});
+
+		//==========================
+		// Set up the synchronization function
+		//==========================
+		this.syncfix.addSyncFunc(() => this.synchronizeAttachments());
+	}
+
+	/**
+	 * When a user joins, attach something to them.
+	 */
+	/*private userJoined(user: MRE.User) {
+		// print the user's name to the console
+		console.log(`${user.name} joined`);
+
+		// attach an item to the user
+		// Assign the return value of CreateFromLibrary() to a variable.
+		const attachment = MRE.Actor.CreateFromLibrary(
+			this.context,
+			{
+				resourceId: 'artifact:1725170080645382585',
+				actor: {
+					attachment: {
+						attachPoint: 'head',
+						userId: user.id
+					},
+					transform: {
+						local: {
+							position: { x: 0.0, y: 0.2, z: 0.0 }
+						},
+						global: {
+							scale: { x: 0.4, y: 0.4, z: 0.4 }
+						}
+					}
+				}
+			}
+		);
+
+		// Associate the attachment with the user in the 'attachments' map.
+		this.attachments.set(user.id, attachment);
+
+		//==========================
+		// Let 'syncfix' know a user has joined.
+		//==========================
+		this.syncfix.userJoined();
+	}*/
+
+	/*
+	 * When a user leaves, remove the attachment (if any) and destroy it
+	 */
+	private userLeft(user: MRE.User) {
+		// See if the user has any attachments.
+		if (this.attachments.has(user.id)) {
+			const attachment = this.attachments.get(user.id);
+
+			// Detach the Actor from the user
+			attachment.detach();
+
+			// Destroy the Actor.
+			attachment.destroy();
+
+			// Remove the attachment from the 'attachments' map.
+			this.attachments.delete(user.id);
+		}
 	}
 }
